2c62f69e0b4dacc80cea2529c0d22259e3ec3db2
Vince <vince@ubx.ph>
2019-08-15 18:24:15 +0800

    Implemented web app

---
+++ queuer/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'queuer'
+urlpatterns = [
+    path('queues/<int:queue_id>/', views.detail, name='detail'),
+    path('api/increment/', views.increment, name='increment'),
+    path('', views.listing, name='listing'),
+]
