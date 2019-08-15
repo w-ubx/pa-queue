b55ad0cb027e23cffb37dbe006d9baa196ddc469
Wilson Talento <wilson@ubx.ph>
2019-08-15 21:21:56 +0800

    Added wallet feature

--- queuer/admin.py
+++ queuer/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Queue
+from .models import Queue, Wallet
 
 admin.site.register(Queue)
+admin.site.register(Wallet)
--- queuer/models.py
+++ queuer/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Queue(models.Model):
@@ -12,3 +13,13 @@ class Queue(models.Model):
         self.current_number += 1
         return self.current_number
 
+class Wallet(models.Model):
+    user = models.ForeignKey(User, on_delete='models.CASCADE')
+    value = models.FloatField(default=0.0)
+
+    def __str__(self):
+        return self.user.username
+
+    def payment(self):
+        self.value -= 2
+        self.save()
